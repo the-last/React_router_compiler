2f409d6f4c6427baa5e6a00db49c5ffe7259e957
the-last <1039107092@qq.com>
2019-10-27 12:08:39 +0800

    初始化-路由编译工具.

---
+++ routes.jsx
@@ -0,0 +1,35 @@
+<Router>
+            <Route
+                path="/hello"
+                component={HELLO}
+            >
+            
+            <Route
+                path="/water"
+                component={WATER}
+            />
+            </Route>
+            <Route
+                path="/market"
+                component={MARKET}
+            >
+            
+            <Route
+                path="/vegetable"
+                component={VEGE}
+            >
+            
+            <Route
+                path="/tomato"
+                component={TOMATO}
+            />
+            </Route>
+            </Route>
+            <Route
+                path="/food"
+                component={FOOD}
+            />
+            <Route
+                path="/banana"
+                component={BANANA}
+            /></Router>
